5e03b106860462d4cc8851dceff1a44fb2d62da1
MusicDino <140427367+MusicDino@users.noreply.github.com>
2023-07-28 19:35:51 +0200

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,11 @@
+(async function main() {
+    const source = document.getElementById("entry-template").innerHTML;
+    const template = Handlebars.compile(source);
+    const response = await fetch ("https://pokeapi.co/api/v2/pokemon-color/yellow")
+    const data = await response.json()
+    const pokemons = data.pokemon_species.slice (0, 20)
+    const context = { pokemons: };
+    const html = template(context);
+
+    document.querySelector("body").innerHTML = html
+}) ()
